b28a2f63c8cab189b87f4b8b5c841a76adb73586
YZhLu <luiz.zito@icloud.com>
2025-02-12 20:48:16 -0300

    Update Taskanswer.ts

--- types/Taskanswer.ts
+++ types/Taskanswer.ts
@@ -1,16 +1,16 @@
-import type { Task } from './Task';
-import { TimeSegment } from './TimeSegment';
+import type { Task } from "./Task";
+import { TimeSegment } from "./TimeSegment";
 
 export type Taskanswer = {
-	essayAnswer: string;
-	alternativeAnswersIds: string[];
-	id: string;
-	taskId: string;
-	studentId: string;
-	feedback?: string;
-	task?: Task;
-	timeSegments?: TimeSegment[];
-	classroomId: string;
-	totalDuration?: number;
-	views?: number;
+  essayAnswer: string;
+  alternativeAnswersIds: string[];
+  id: string;
+  taskId: string;
+  studentId: string;
+  feedback?: string;
+  task?: Task;
+  timeSegments?: TimeSegment[];
+  classroomId: string;
+  totalDuration?: number;
+  views?: number;
 };
