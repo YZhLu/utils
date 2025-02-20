8f451dac9d73357b0fe37b506f4396df0caf9ef2
YZhLu <luiz.zito@icloud.com>
2025-02-20 10:35:39 -0300

    Update Profile.ts

--- types/Profile.ts
+++ types/Profile.ts
@@ -1,25 +1,31 @@
 export type Profile = {
-	id: string;
-	bio: string | null;
-	picture: string | null;
-	//
-	level: number;
-	stars: number[];
-	stage: number;
-	//
-	streakCount: number;
-	streakStartDate: string;
-	lastStreakCheckpointDate: string;
-	belt: string;
-	points: number;
-	//
-	stamina: number; // volume
-	discipline: number; // frequencia
-	knowledge: number; // intensidade
-	speed: number; // densidade
-	runes: string[];
-	badges: string[];
-	spells: string[];
-	studentId: string;
-	classroomId: string;
+  id: string;
+  bio: string | null;
+  picture: string | null;
+  //
+  level: number;
+  stars: number[];
+  stage: number;
+  //
+  streakCount: number;
+  streakStartDate: string;
+  lastStreakCheckpointDate: string;
+  belt: string;
+  points: number;
+  //
+  stamina: number; // volume
+  discipline: number; // frequencia
+  strength?: number;  // intensidade
+  speed: number; // densidade
+  agile?: number; // variedade
+
+  taskCount?: number;
+  hitCount?: number;
+  totalTime?: number;
+
+  runes: string[];
+  badges: string[];
+  spells: string[];
+  studentId: string;
+  classroomId: string;
 };
